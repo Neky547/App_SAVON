788d1b570d827e9bcc8b9677e633ee07c5bd877a
Neky547 <nekenarab55@gmail.com>
2026-03-13 16:26:29 +0100

    Gestion recettes

--- src/app/pages/recipe-manager-page/recipe-manager-page.ts
+++ src/app/pages/recipe-manager-page/recipe-manager-page.ts
@@ -3,27 +3,27 @@ import { Recette } from '../../models/recette.model';
 import { RecetteService } from '../../services/recette.service';
 import { CommonModule } from '@angular/common';
 @Component({
-selector: 'app-recipe-manager-page',
-imports: [CommonModule],
-templateUrl: './recipe-manager-page.html',
-styleUrl: './recipe-manager-page.css',
+  selector: 'app-recipe-manager-page',
+  imports: [CommonModule],
+  templateUrl: './recipe-manager-page.html',
+  styleUrl: './recipe-manager-page.css',
 })
 export class RecipeManagerPage implements OnInit {
-public recettes: Recette[] = []
-constructor(private recetteService: RecetteService) {}
-ngOnInit(): void {
-this.chargerRecettes();
-}
-chargerRecettes(): void {
-this.recetteService.getRecettes().subscribe({
-next: (data) => this.recettes = data,
-error: (err) => console.error("Erreur API", err)
-})
-}
-supprimerRecette(id: number): void {
-if (confirm("Supprimer cette recette ?")) {
-this.recetteService.deleteRecette(id).subscribe( () =>
-this.chargerRecettes());
-}
-}
+  public recettes: Recette[] = []
+  constructor(private recetteService: RecetteService) { }
+  ngOnInit(): void {
+    this.chargerRecettes();
+  }
+  chargerRecettes(): void {
+    this.recetteService.getRecettes().subscribe({
+      next: (data) => this.recettes = data,
+      error: (err) => console.error("Erreur API", err)
+    })
+  }
+  supprimerRecette(id: number): void {
+    if (confirm("Supprimer cette recette ?")) {
+      this.recetteService.deleteRecette(id).subscribe(() =>
+        this.chargerRecettes());
+    }
+  }
 }
