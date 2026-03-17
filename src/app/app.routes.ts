8f99a9c20164217da7423c83d0e2852568eb35a1
Neky547 <nekenarab55@gmail.com>
2026-03-17 09:17:01 +0100

    Page de connexion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { UsersManagerPage } from './pages/users-manager-page/users-manager-page'
 import { IngredientsManagerPage } from './pages/ingredients-manager-page/ingredients-manager-page';
 import { AboutPage } from './pages/about-page/about-page';
 import { LegalNoticePage } from './pages/legal-notice-page/legal-notice-page';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
 
@@ -19,16 +20,18 @@ export const routes: Routes = [
 { path: 'login', component: LoginPage },
 { path: 'subscribe', component: SubscribePage},
 // Compte utilisateur :
-{ path: 'account', component: AccountManagerPage },
+{ path: 'account', component: AccountManagerPage,canActivate: [authGuard] },
 // Recettes :
 { path: 'recipe-calculator', component: RecipeCalculatorPage },
-{ path: 'recipe-manager', component: RecipeManagerPage},
+{ path: 'recipe-manager', component: RecipeManagerPage, canActivate: [authGuard]},
 // Administration - Gestion :
-{ path: 'users-manager', component: UsersManagerPage },
-{ path: 'ingredients-manager', component: IngredientsManagerPage},
+{ path: 'users-manager', component: UsersManagerPage, canActivate: [authGuard] },
+{ path: 'ingredients-manager', component: IngredientsManagerPage, canActivate: [authGuard]},
 // Mentions légales :
 { path: "legal-notice", component: LegalNoticePage },
 // A propos (redirection vers home en cas d'url invalide):
-{ path: "about", component: AboutPage } // Toujours mis en dernier !
+{ path: "about", component: AboutPage },
+// Redirection par défaut vers home (en cas d'url invalide) :
+{path: '**', redirectTo: 'home'} 
 
 ];
