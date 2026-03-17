fb3b9d89b97e1ca6ab5f3c77620365b5b338690d
Neky547 <nekenarab55@gmail.com>
2026-03-17 11:37:04 +0100

    Login page

--- src/app/pages/subscribe-page/subscribe-page.ts
+++ src/app/pages/subscribe-page/subscribe-page.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-subscribe-page',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './subscribe-page.html',
   styleUrl: './subscribe-page.css',
 })
 export class SubscribePage {
-
+  
 }
