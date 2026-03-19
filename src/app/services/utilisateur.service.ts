a269401ea63f928a82147a07aebb9d0920e1355c
Neky547 <nekenarab55@gmail.com>
2026-03-19 10:51:46 +0100

    Gestion des utilisateurs

--- src/app/services/utilisateur.service.ts
+++ src/app/services/utilisateur.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -7,5 +8,29 @@ export class UtilisateurService {
   // URL de base de notre API pour les utilisateurs
   private readonly API_URL_UTILISATEUR = 'http://localhost:8080/api-savon/v1/utilisateur';
   
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getUtilisateurs() {
+    return this.http.get(this.API_URL_UTILISATEUR);
+  }
+  
+  getUtilisateurById(id: number) {
+    return this.http.get(`${this.API_URL_UTILISATEUR}/${id}`);
+  }
+
+  /**
+  * Enregistre un nouvel utilisateur.
+  * @param utilisateur - L'objet utilisateur à enregistrer.
+  * @returns Un Observable contenant l'utilisateur enregistré.
+  */
+
+  createUtilisateur(utilisateur: any) {
+    return this.http.post(this.API_URL_UTILISATEUR, utilisateur);
+  }
+
+  /**
+  * Supprime un utilisateur à partir de son id
+  */
+  deleteUtilisateur(id: number) {
+    return this.http.delete(`${this.API_URL_UTILISATEUR}/${id}`)
+   }
 }
