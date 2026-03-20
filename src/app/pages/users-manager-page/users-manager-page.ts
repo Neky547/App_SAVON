31b9981581ccab004d0458692f9095be363d4883
Neky547 <nekenarab55@gmail.com>
2026-03-20 15:20:38 +0100

    CRUD utilisateurs

--- src/app/pages/users-manager-page/users-manager-page.ts
+++ src/app/pages/users-manager-page/users-manager-page.ts
@@ -1,11 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-users-manager-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './users-manager-page.html',
   styleUrl: './users-manager-page.css',
 })
 export class UsersManagerPage {
-
+  public users: any[] = [
+    { id: 1, nom: 'Admin', email: 'admin@email.com', role: 'Administrateur' },
+    { id: 2, nom: 'Nekena', email: 'utilisateur@email.com', role: 'Utilisateur' },
+    { id: 3, nom: 'John Doe', email: 'johndoe@email.com', role: 'Utilisateur' },
+  ];
+  createUser() {
+    // Logique pour créer un nouvel utilisateur
+    console.log('Créer un nouvel utilisateur');
+  }
+  editUser(user: any) {
+    // Logique pour éditer les informations de l'utilisateur
+    console.log('Éditer l\'utilisateur', user);
+  }
+  deleteUser(user: any) {
+    // Logique pour supprimer l'utilisateur
+    console.log('Supprimer l\'utilisateur', user);
+  }
 }
